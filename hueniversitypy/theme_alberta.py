719396eeba57573bba71d1217a7e50aabe79ac12
Reiko Okamoto <reiko.okamoto17@gmail.com>
2020-02-28 10:02:12 -0800

    Add alberta docstring, edit README

---
+++ hueniversitypy/theme_alberta.py
@@ -0,0 +1,21 @@
+def theme_alberta(palette='alpha'):
+    """ 
+    Applies a University of Alberta theme to all subsequential altair plot objects so they are displayed with the U of A visual identity. 
+    Four palettes based on the U of A visual identity guidelines can be selected: 'alpha', 'beta', 'gamma' and 'delta'.
+	
+    Parameters
+    ----------
+    palette : 'alpha', 'beta', 'gamma' or 'delta'
+    	specifies which colour palette defined in the visual identity documentation is used. 
+	
+    Returns
+    -------
+    altair plot : altair.vegalite.v4.api.Chart
+    	an altair plot with the U of A visual identity colour theme applied. 
+
+    Example
+    ----------
+    >>> alt.themes.register('theme_alberta', theme_alberta)
+    >>> alt.themes.enable('theme_alberta')
+
+    """
