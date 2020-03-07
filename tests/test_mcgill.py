b4732a3499d5ebb1ab4e192f71b3645abcae7a69
Simardeep Kaur <simardeep.kaur.jeji@gmail.com>
2020-03-07 12:07:01 -0700

    Updating theme mcgill

--- tests/test_mcgill.py
+++ tests/test_mcgill.py
@@ -27,6 +27,8 @@ def test_chart_object():
 def test_colours():
     ''' A function that tests the correct colours an imported into the 
     altair chart based on the theme'''
+    
+    # creating data to plot for tests
     data = pd.melt(pd.DataFrame({'A': [1, 4, 6, 8, 3, 6, 7], 
                      'B':[2, 3, 7, 4, 8, 4, 9], 
                      'C':[3, 6, 3, 5, 8, 2, 8] }))
@@ -48,6 +50,7 @@ def test_font_type():
     #
     ''' A function that tests the correct fonts are imported into the 
     altair chart based on the theme'''
+    
     # creating data to plot for tests
     data = pd.melt(pd.DataFrame({'A': [1, 4, 6, 8, 3, 6, 7], 
                      'B':[2, 3, 7, 4, 8, 4, 9], 
@@ -65,10 +68,10 @@ def test_font_type():
     
     plot_dict = test_plot.to_dict()
     #print(plot_dict['config']['axisX']['titleFont'])
-    assert plot_dict['config']['title']['font'] == 'Lato', 'Test 1 fail'
-    assert plot_dict['config']['axisX']['labelFont'] == 'Lato', 'Test 2 fail'
+    assert plot_dict['config']['title']['font'] == 'Lato'
+    assert plot_dict['config']['axisX']['labelFont'] == 'Lato'
     #assert plot_dict['config']['axisX']['titleFont'] == 'Lato', 'Test 3 fail'
-    assert plot_dict['config']['axisY']['labelFont'] == 'Lato', 'Test 4 fail'
+    assert plot_dict['config']['axisY']['labelFont'] == 'Lato'
     #assert plot_dict['config']['axisY']['titleFont'] == 'Lato', 'Test 5 fail'
     
 def test_font_size():
