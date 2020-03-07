904362169d97b67989fadb8c71edd90dfed6d6d1
moorhou7 <evelyn.moorhouse@mail.utoronto.ca>
2020-03-07 08:56:45 -0800

    added feedback to test and updated theme toronto function with changes from mcgill

--- hueniversitypy/theme_toronto.py
+++ hueniversitypy/theme_toronto.py
@@ -19,8 +19,10 @@ def theme_toronto():
     >>> alt.themes.register('theme_toronto', theme_toronto)
     >>> alt.themes.enable('theme_toronto')
 
+    attribution to Sergio Sanchez 
+    https://towardsdatascience.com/consistently-beautiful-visualizations-with-altair-themes-c7f9f889602
+    
     """
-    # Typography
     # note U of T fonts are not open source, so most comprable fonts have been used for this package
     font = "Tahoma"
     # note U of T fonts are not open source, so most comprable fonts have been used for this package
@@ -37,41 +39,23 @@ def theme_toronto():
             "title": {
                 "fontSize": 18,
                 "font": font,
-                "anchor": "start", # equivalent of left-aligned.
+                "anchor": "start", 
                 "fontColor": "#000000"
             },
             "axisX": {
-                "domain": True,
-                "domainColor": axisColor,
-                "domainWidth": 1,
-                "grid": False,
                 "labelFont": labelFont,
                 "labelFontSize": 12,
-                "labelAngle": 0, 
-                "tickColor": axisColor,
-                "tickSize": 5, # default, including it just to show you can change it
                 "titleFont": font,
                 "titleFontSize": 12,
-                "titlePadding": 10, # guessing, not specified in styleguide
                 "title": "X Axis Title (units)", 
             },
             "axisY": {
-                "domain": False,
-                "grid": True,
-                "gridColor": gridColor,
-                "gridWidth": 1,
                 "labelFont": labelFont,
                 "labelFontSize": 12,
-                "labelAngle": 0, 
-                "ticks": False, # even if you don't have a "domain" you need to turn these off.
                 "titleFont": font,
                 "titleFontSize": 12,
-                "titlePadding": 10, # guessing, not specified in styleguide
                 "title": "Y Axis Title (units)", 
-                # titles are by default vertical left of axis so we need to hack this 
-                "titleAngle": 0, # horizontal
-                "titleY": -10, # move it up
-                "titleX": 18, # move it to the right so it aligns with the labels 
+ 
             },
 
             "range": {
