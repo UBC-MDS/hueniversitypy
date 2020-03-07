64ab0c66a7950542a8320ed94d22bfeb8a86bf55
Simardeep Kaur <simardeep.kaur.jeji@gmail.com>
2020-03-07 00:10:20 -0700

    Updated theme mcgill

--- hueniversitypy/theme_mcgill.py
+++ hueniversitypy/theme_mcgill.py
@@ -6,7 +6,7 @@ def theme_mcgill():
     Returns
     -------
     altair plot : altair.vegalite.v4.api.Chart
-   	an altair plot with the McGill University's visual identity colour theme applied. 
+    an altair plot with the McGill University's visual identity colour theme applied. 
 
     Example
     ----------
@@ -16,7 +16,6 @@ def theme_mcgill():
 
     # Typography
     font = "Lato"
-    # At theme_mcgill it's the same font for all text but it's good to keep them separate in case you want to           #change one later.
     labelFont = "Lato" 
     sourceFont = "Lato"
     # Axes
@@ -30,43 +29,21 @@ def theme_mcgill():
             "title": {
                 "fontSize": 18,
                 "font": font,
-                "anchor": "start", # equivalent of left-aligned.
+                "anchor": "start", 
                 "fontColor": "#000000"
             },
+            # Axes fonts and sizes
             "axisX": {
-                "domain": True,
-                "domainColor": axisColor,
-                "domainWidth": 1,
-                "grid": False,
                 "labelFont": labelFont,
                 "labelFontSize": 12,
-                "labelAngle": 0, 
-                "tickColor": axisColor,
-                "tickSize": 5, # default, including it just to show you can change it
-                "titleFont": font,
-                "titleFontSize": 12,
-                "titlePadding": 10, # guessing, not specified in styleguide
                 "title": "X Axis Title (units)", 
             },
             "axisY": {
-                "domain": False,
-                "grid": True,
-                "gridColor": gridColor,
-                "gridWidth": 1,
                 "labelFont": labelFont,
                 "labelFontSize": 12,
-                "labelAngle": 0, 
-                "ticks": False, # even if you don't have a "domain" you need to turn these off.
-                "titleFont": font,
-                "titleFontSize": 12,
-                "titlePadding": 10, # guessing, not specified in styleguide
                 "title": "Y Axis Title (units)", 
-                # titles are by default vertical left of axis so we need to hack this 
-                "titleAngle": 0, # horizontal
-                "titleY": -10, # move it up
-                "titleX": 18, # move it to the right so it aligns with the labels 
             },
-
+             # Adding colour palette
             "range": {
                 
                 "category": main_palette,
